06995759a3bba6fd690fe99f061fbe47f02db8c9
李恩泽 <lienze87@gmail.com>
2024-05-11 10:52:13 +0800

    fix: 修改画板页面的参数设置

--- src/pages/board/index.vue
+++ src/pages/board/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main-content">
     <div class="form-container">
+      <h3>参考线设置</h3>
       <el-form :inline="true" :model="formData" class="form-inline">
         <el-form-item label="最小值">
           <el-input-number
@@ -27,28 +28,37 @@
             placeholder="请输入"
             clearable />
         </el-form-item>
+        <el-form-item label="方向">
+          <el-select
+            v-model="formData.direction"
+            placeholder="请选择"
+            style="width: 150px">
+            <el-option label="水平线" :value="'horizon'"></el-option>
+            <el-option label="垂直线" :value="'vertical'"></el-option>
+          </el-select>
+        </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">确定</el-button>
+          <el-button type="danger" @click="onReset">重置</el-button>
         </el-form-item>
       </el-form>
+      <p>请在画板内用鼠标进行绘制</p>
       <div class="my-board" ref="myBoard"></div>
-    </div>
-    <div class="white-board">
-      <WhiteBoard />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import { ref, onMounted, computed } from "vue";
-  import WhiteBoard from "@/components/WhiteBoard.vue";
+  import { initCanvasDraw } from "@/utils/canvasDraw";
 
   const FONT_SIZE = 24;
 
   const formData = ref({
     min: 0,
-    max: 12,
-    step: 2,
+    max: 6,
+    step: 1,
+    direction: "vertical",
   });
   const dataList = ref([]);
   const percentList = computed(() => {
@@ -71,11 +81,11 @@
     return result;
   }
 
-  function drawAxis() {
+  function drawXAxis() {
     const width = myCanvas.width;
     const height = myCanvas.height;
     const paddingLeft = 50;
-    const paddingTop = Math.floor(height / 2);
+    const paddingTop = Math.floor(height * 0.9);
 
     ctx.fillStyle = "#000";
     ctx.beginPath();
@@ -95,8 +105,37 @@
       ctx.stroke();
       ctx.fillText(
         dataList.value[index].toString(),
-        offsetLeft,
-        paddingTop + 30
+        offsetLeft - FONT_SIZE * 0.2,
+        paddingTop + FONT_SIZE
+      );
+    });
+  }
+
+  function drawYAxis() {
+    const width = myCanvas.width;
+    const height = myCanvas.height;
+    const paddingLeft = Math.floor(width * 0.1);
+    const paddingTop = 50;
+
+    ctx.fillStyle = "#000";
+    ctx.beginPath();
+
+    // x轴
+    ctx.moveTo(paddingLeft, paddingTop);
+    ctx.lineTo(paddingLeft, height - paddingTop);
+    ctx.stroke();
+
+    // 绘制刻度
+    percentList.value.forEach((ele: number, index: number) => {
+      let offsetTop = paddingTop + Math.floor((height - 2 * paddingTop) * ele);
+
+      ctx.moveTo(paddingLeft, offsetTop);
+      ctx.lineTo(paddingLeft + 30, offsetTop);
+      ctx.stroke();
+      ctx.fillText(
+        dataList.value[index].toString(),
+        paddingLeft - FONT_SIZE,
+        offsetTop + FONT_SIZE * 0.2
       );
     });
   }
@@ -109,7 +148,11 @@
   function onSubmit() {
     dataList.value = getDataList();
     onReset();
-    drawAxis();
+    if (formData.value.direction === "vertical") {
+      drawYAxis();
+    } else {
+      drawXAxis();
+    }
   }
 
   const myBoard = ref();
@@ -120,12 +163,17 @@
   onMounted(() => {
     if (myBoard.value) {
       // Fill Window Width and Height
-      myCanvas.width = myBoard.value.clientWidth || 500;
-      myCanvas.height = myBoard.value.clientHeight || 200;
+      // A4	21.0 x 29.7厘米(8.27 x 11.69英寸)
+      myCanvas.width = myBoard.value.clientWidth || 210;
+      myCanvas.height = myBoard.value.clientHeight || 297;
 
       myBoard.value.appendChild(myCanvas);
       ctx = myCanvas.getContext("2d");
       ctx.font = `${FONT_SIZE}px serif`;
+      ctx.lineWidth = 1;
+      if (ctx) {
+        initCanvasDraw(myCanvas, ctx);
+      }
     }
     onSubmit();
   });
@@ -140,6 +188,9 @@
     height: calc(100vh - 100px);
   }
   .my-board {
+    width: 100%;
+    height: 594px;
     border: 1px solid green;
+    cursor: crosshair;
   }
 </style>
---
+++ src/utils/canvasDraw.ts
@@ -0,0 +1,96 @@
+export function initCanvasDraw(
+  myCanvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) {
+  // Set Background Color
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+
+  // Mouse Event Handlers
+
+  let isDown = false;
+  let canvasX, canvasY;
+
+  const mouseDraw = {
+    mousedown: function (evt: MouseEvent) {
+      isDown = true;
+      ctx.beginPath();
+      const scrollTop = window.scrollY;
+      canvasX = evt.pageX - myCanvas.getBoundingClientRect().left;
+      canvasY = evt.pageY - myCanvas.getBoundingClientRect().top - scrollTop;
+      ctx.moveTo(canvasX, canvasY);
+    },
+    mousemove: function (evt: MouseEvent) {
+      if (isDown !== false) {
+        const scrollTop = window.scrollY;
+        canvasX = evt.pageX - myCanvas.getBoundingClientRect().left;
+        canvasY = evt.pageY - myCanvas.getBoundingClientRect().top - scrollTop;
+        ctx.lineTo(canvasX, canvasY);
+        ctx.strokeStyle = "#000";
+        ctx.stroke();
+      }
+    },
+    mouseup: function (evt: MouseEvent) {
+      isDown = false;
+      ctx.closePath();
+    },
+    mouseleave: function (evt: MouseEvent) {
+      isDown = false;
+      ctx.closePath();
+    },
+  };
+
+  // Mouse Events
+  myCanvas.addEventListener("mousedown", mouseDraw.mousedown, false);
+  myCanvas.addEventListener("mouseup", mouseDraw.mouseup, false);
+  myCanvas.addEventListener("mousemove", mouseDraw.mousemove, false);
+  myCanvas.addEventListener("mouseleave", mouseDraw.mouseleave, false);
+
+  // Touch Events Handlers
+  const touchDraw = {
+    started: false,
+    start: function (evt: TouchEvent) {
+      ctx.beginPath();
+      const scrollTop = window.scrollY;
+      canvasX = evt.touches[0].pageX - myCanvas.getBoundingClientRect().left;
+      canvasY =
+        evt.touches[0].pageY - myCanvas.getBoundingClientRect().top - scrollTop;
+
+      ctx.moveTo(canvasX, canvasY);
+      this.started = true;
+    },
+    move: function (evt: TouchEvent) {
+      evt.preventDefault();
+
+      if (this.started) {
+        const scrollTop = window.scrollY;
+        canvasX = evt.touches[0].pageX - myCanvas.getBoundingClientRect().left;
+        canvasY =
+          evt.touches[0].pageY -
+          myCanvas.getBoundingClientRect().top -
+          scrollTop;
+
+        ctx.lineTo(canvasX, canvasY);
+        ctx.strokeStyle = "#000";
+        ctx.stroke();
+      }
+    },
+    end: function (evt: TouchEvent) {
+      this.started = false;
+    },
+  };
+
+  // Touch Events
+  myCanvas.addEventListener("touchstart", touchDraw.start, false);
+  myCanvas.addEventListener("touchend", touchDraw.end, false);
+  myCanvas.addEventListener("touchmove", touchDraw.move, false);
+
+  // Disable Page Move
+  document.body.addEventListener(
+    "touchmove",
+    function (evt) {
+      evt.stopPropagation();
+    },
+    false
+  );
+}
