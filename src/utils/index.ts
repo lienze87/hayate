eec5574e5d25a579262857a5826ff27b26137720
李恩泽 <lienze87@gmail.com>
2024-05-14 18:01:49 +0800

    feat(util): 新增获取文件后缀名函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,8 @@ export function randomHex() {
   // 0xffffff=16777215
   return Math.floor(Math.random() * 16777215).toString(16);
 }
+
+// 获取文件拓展名
+export function getFileExtension(filename: string) {
+  return filename.split(".").pop();
+}
