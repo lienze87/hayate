ccaf2f6c280878bc39f2fde332977177f436195c
李恩泽 <lienze87@gmail.com>
2024-05-25 20:51:01 +0800

    feat: 新增pinia和pinia持久化插件

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import 'element-plus/dist/index.css';
 import '@/assets/styles/style.css';
 import App from './App.vue';
 import router from './router';
+import { store } from './store';
 
 const app = createApp(App);
 
 app.use(ElementPlus);
+app.use(store);
 app.use(router);
 app.mount('#app');
