c52ef70ebee8d0f334c5836fcd12dbff8c440be7
李恩泽 <lienze87@gmail.com>
2024-06-06 16:38:54 +0800

    fix(page): 修改地图页面的小球发射角度和碰撞检测

--- src/pages/game/map.vue
+++ src/pages/game/map.vue
@@ -3,8 +3,13 @@
 </template>
 <script lang="ts" setup>
 import type { FederatedPointerEvent } from 'pixi.js';
-import { Application, Assets, Container, Graphics, Rectangle, Sprite, Text } from 'pixi.js';
+import { Application, Container, Graphics, Point, Text } from 'pixi.js';
 import { onMounted } from 'vue';
+
+interface PhysicGraphics extends Graphics {
+  acceleration?: Point;
+  mass?: number;
+}
 
 let app: Application = null;
 
@@ -15,81 +20,162 @@ async function initApp() {
 
   body.appendChild(app.canvas);
 
+  const rectWidth = 100;
+  const rotationSpeed = Math.PI / 18;
+  let rotationAngle = 0;
   const container = new Container();
-
+  container.x = (app.screen.width - rectWidth / 2) / 2;
+  container.y = app.screen.height - rectWidth;
   app.stage.addChild(container);
 
-  const rectPadding = 500;
-  const rect = new Graphics({
-    x: app.screen.width / 2 - rectPadding / 2,
-    y: app.screen.height / 2 - rectPadding / 2,
-  })
-    .rect(0, 0, rectPadding, rectPadding)
-    .stroke({ color: 'rgb(0,255,0)', width: 10, alignment: 0 });
-  app.stage.addChild(rect);
-  rect.eventMode = 'static';
-  rect.cursor = 'pointer';
-  rect.on('pointerover', onMouseOver);
-  rect.on('pointerout', onMouseOut);
-
-  let hoverTarget: Container | null = null;
-
-  function onMouseOver(event: FederatedPointerEvent) {
-    hoverTarget = event.target;
-    hoverTarget.tint = Math.random() * 0xff0000;
-  }
-
-  function onMouseOut() {
-    if (hoverTarget) {
-      hoverTarget.tint = undefined;
-      hoverTarget = null;
-    }
-  }
-
-  const texture = await Assets.load('/vite.svg');
-  console.log('texture ', texture.width, texture.height);
-
-  const icon = new Sprite(texture);
-  app.stage.addChild(icon);
-
-  icon.anchor.set(0.5);
-  icon.x = app.screen.width / 2;
-  icon.y = app.screen.height / 2;
-  icon.eventMode = 'static';
-  icon.cursor = 'pointer';
-  icon.on('pointerdown', onDragStart);
-
-  // W,A,S,D控制方向
-  let offset = { x: 1, y: 0 };
-
-  app.ticker.add(() => {
-    icon.x += offset.x;
-    icon.y += offset.y;
-    if (icon.x < rect.x) {
-      offset = { x: 1, y: 0 };
-    } else if (icon.x > rect.x + rect.width) {
-      offset = { x: -1, y: 0 };
-    }
-    if (icon.y < rect.y) {
-      offset = { x: 0, y: 1 };
-    } else if (icon.y > rect.y + rect.height) {
-      offset = { x: 0, y: -1 };
-    }
-  });
-
   const basicText = new Text({
-    text: 'W A S D控制方向',
+    text: 'Q键和E键旋转方向,空格发射小球',
     style: {
       fill: 0xff0000,
     },
   });
 
-  basicText.x = rect.x;
-  basicText.y = rect.y - 50;
+  basicText.x = 50;
+  basicText.y = 50;
 
   app.stage.addChild(basicText);
 
+  const rect = new Graphics().rect(0, 0, rectWidth / 2, rectWidth * 3).fill({ color: 0x00ff00 });
+  rect.pivot = new Point(rectWidth / 4, rectWidth);
+  container.addChild(rect);
+
+  const ball = new Graphics().circle(0, 0, rectWidth / 4).fill(0xff0000);
+  const ballToRectCenter = rectWidth + 10;
+  container.addChild(ball);
+  ball.x = 0;
+  ball.y = -ballToRectCenter;
+  ball.zIndex = -1;
+
+  const bullet: PhysicGraphics = new Graphics().circle(0, 0, rectWidth / 4).fill(0x0000ff);
+  bullet.x = ball.getGlobalPosition().x;
+  bullet.y = ball.getGlobalPosition().y;
+  bullet.alpha = 0;
+  bullet.acceleration = new Point(0, 0);
+  bullet.mass = 3;
+  app.stage.addChild(bullet);
+
+  function fireGun() {
+    ball.alpha = 0;
+    bullet.x = ball.getGlobalPosition().x;
+    bullet.y = ball.getGlobalPosition().y;
+
+    const vCollision = new Point(container.x - bullet.x, container.y - bullet.y);
+    const distance = ballToRectCenter;
+    const vCollisionNorm = new Point(vCollision.x / distance, vCollision.y / distance);
+
+    const impulse = 3 * 9.81;
+    bullet.acceleration = new Point(-impulse * vCollisionNorm.x, -impulse * vCollisionNorm.y);
+    bullet.alpha = 1;
+  }
+
+  function circleIntersectRect(cx: number, cy: number, cr: number, rx: number, ry: number, rw: number, rh: number) {
+    let testX = cx;
+    let testY = cy;
+
+    if (cx < rx) {
+      testX = rx;
+    } else if (cx > rx + rw) {
+      testX = rx + rw;
+    }
+
+    if (cy < ry) {
+      testY = ry;
+    } else if (cy > ry + rh) {
+      testY = ry + rh;
+    }
+
+    const distance = Math.sqrt((cx - testX) * (cx - testX) + (cy - testY) * (cy - testY));
+
+    return distance <= cr;
+  }
+
+  const impulsePower = 5;
+  function collisionResponse(rect: PhysicGraphics, circle: PhysicGraphics) {
+    if (!rect || !circle) {
+      return new Point(0);
+    }
+
+    // 以方块重心点为目标点
+    const rectCenter = new Point(rect.x + rect.width * 0.5, rect.y + rect.height * 0.5);
+
+    // 计算碰撞方向
+    const vCollision = new Point(circle.x - rectCenter.x, circle.y - rectCenter.y);
+
+    const distance = Math.sqrt(
+      (circle.x - rectCenter.x) * (circle.x - rectCenter.x) + (circle.y - rectCenter.y) * (circle.y - rectCenter.y),
+    );
+
+    const vCollisionNorm = new Point(vCollision.x / distance, vCollision.y / distance);
+
+    // 相对速度为两向量终点相连
+    const vRelativeVelocity = new Point(
+      rect.acceleration.x - circle.acceleration.x,
+      rect.acceleration.y - circle.acceleration.y,
+    );
+
+    // 速度为相对速度在单位向量方向上的投影，即两向量的点乘
+    const speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
+
+    // 计算单位质量的动能
+    const impulse = (impulsePower * speed) / (rect.mass + circle.mass);
+
+    return new Point(impulse * vCollisionNorm.x, impulse * vCollisionNorm.y);
+  }
+
+  const wall: PhysicGraphics = new Graphics().rect(0, 0, 500, 50).fill(0xf000f0);
+  wall.x = app.screen.width / 2 - 250;
+  wall.y = 100;
+  wall.acceleration = new Point(0, 0);
+  wall.mass = 5;
+  app.stage.addChild(wall);
+
+  wall.eventMode = 'static';
+  wall.cursor = 'pointer';
+  wall.on('pointerdown', onDragStart);
+
+  app.ticker.add(() => {
+    if (circleIntersectRect(bullet.x, bullet.y, bullet.width * 0.5, wall.x, wall.y, wall.width, wall.height)) {
+      const collisionPush = collisionResponse(wall, bullet);
+      console.log('new acceleration', collisionPush);
+
+      wall.tint = 0x456859;
+      bullet.acceleration.set(collisionPush.x * bullet.mass, collisionPush.y * bullet.mass);
+    }
+  });
+
+  const restitution = 0.99;
+  app.ticker.add((time) => {
+    const delta = time.deltaTime;
+
+    if (Math.abs(bullet.acceleration.x) > 0.01 || Math.abs(bullet.acceleration.y) > 0.01) {
+      bullet.acceleration.set(bullet.acceleration.x * restitution, bullet.acceleration.y * restitution);
+    } else {
+      bullet.acceleration.set(0, 0);
+    }
+
+    if (bullet.x < 0 || bullet.x > app.screen.width - bullet.width / 2) {
+      bullet.acceleration.x = -bullet.acceleration.x;
+    }
+    if (bullet.y < 0 || bullet.y > app.screen.height - bullet.height / 2) {
+      bullet.acceleration.y = -bullet.acceleration.y;
+    }
+
+    if (Math.abs(rotationAngle) > 0) {
+      container.rotation += rotationAngle;
+      rotationAngle = 0;
+    }
+
+    bullet.x += bullet.acceleration.x * delta;
+    bullet.y += bullet.acceleration.y * delta;
+  });
+
   let dragTarget: Container | null = null;
+  const beginPosition = new Point(0, 0);
 
   app.stage.eventMode = 'static';
   app.stage.hitArea = app.screen;
@@ -99,12 +185,18 @@ async function initApp() {
   function onDragStart(event: FederatedPointerEvent) {
     dragTarget = event.target;
     dragTarget.alpha = 0.5;
+    beginPosition.x = event.clientX - dragTarget.x;
+    beginPosition.y = event.clientY - dragTarget.y;
     app.stage.on('pointermove', onDragMove);
   }
 
   function onDragMove(event: FederatedPointerEvent) {
     if (dragTarget) {
-      dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+      dragTarget.parent.toLocal(
+        new Point(event.globalX - beginPosition.x, event.globalY - beginPosition.y),
+        null,
+        dragTarget.position,
+      );
     }
   }
 
@@ -116,82 +208,21 @@ async function initApp() {
     }
   }
 
-  const bug = await Assets.load('/textures/maggot_tiny.png');
-  const dudeSprites = new Container();
-  app.stage.addChild(dudeSprites);
-
-  const dude = new Sprite(bug);
-  dude.anchor.set(0.5);
-  dude.scale.set(0.8 + Math.random() * 0.3);
-
-  dude.x = Math.random() * app.screen.width;
-  dude.y = Math.random() * app.screen.height;
-
-  // 上色
-  dude.tint = Math.random() * 0x808080;
-
-  dude.direction = Math.random() * Math.PI * 2;
-  dude.turningSpeed = Math.random() - 0.8;
-  dude.speed = (2 + Math.random() * 2) * 0.2;
-  dude.offset = Math.random() * 100;
-  dudeSprites.addChild(dude);
-
-  const dudeBoundsPadding = 100;
-  const dudeBounds = new Rectangle(
-    -dudeBoundsPadding,
-    -dudeBoundsPadding,
-    app.screen.width + dudeBoundsPadding * 2,
-    app.screen.height + dudeBoundsPadding * 2,
-  );
-
-  let tick = 0;
-
-  app.ticker.add(() => {
-    dude.scale.y = 0.95 + Math.sin(tick + dude.offset) * 0.05;
-    dude.direction += dude.turningSpeed * 0.01;
-    dude.x += Math.sin(dude.direction) * (dude.speed * dude.scale.y);
-    dude.y += Math.cos(dude.direction) * (dude.speed * dude.scale.y);
-    dude.rotation = -dude.direction + Math.PI;
-
-    if (dude.x < dudeBounds.x) {
-      dude.x += dudeBounds.width;
-      dude.tint = Math.random() * 0x808080;
-    } else if (dude.x > dudeBounds.x + dudeBounds.width) {
-      dude.x -= dudeBounds.width;
-      dude.tint = Math.random() * 0x808080;
-    }
-    if (dude.y < dudeBounds.y) {
-      dude.y += dudeBounds.height;
-      dude.tint = Math.random() * 0x808080;
-    } else if (dude.y > dudeBounds.y + dudeBounds.height) {
-      dude.y -= dudeBounds.height;
-      dude.tint = Math.random() * 0x808080;
-    }
-    tick += 0.1;
-  });
-
   function handleKeyDown(event: KeyboardEvent) {
     console.log(event.code);
     switch (event.code) {
-      case 'KeyW':
-        offset = { x: 0, y: -1 };
+      case 'KeyQ':
+        rotationAngle = -rotationSpeed;
         break;
-      case 'KeyA':
-        offset = { x: -1, y: 0 };
-        break;
-      case 'KeyS':
-        offset = { x: 0, y: 1 };
-        break;
-      case 'KeyD':
-        offset = { x: 1, y: 0 };
+      case 'KeyE':
+        rotationAngle = rotationSpeed;
         break;
       case 'Space':
-        offset = { x: 0, y: 0 };
+        fireGun();
         break;
       default:
         break;
     }
-    console.log(offset);
   }
   window.addEventListener('keydown', handleKeyDown);
 }
