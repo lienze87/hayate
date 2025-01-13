1ba61fad5678b00a109fa189e7b7bb4864868803
李恩泽 <lienze87@gmail.com>
2025-01-13 22:35:07 +0800

    fix: 修改element-plus的组件语言

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="zhCn">
+    <router-view />
+  </el-config-provider>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+</script>
 <style lang="scss" scoped></style>
